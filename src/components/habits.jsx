f9e9c09f83748492d3b75480592e93da33a0b259
YeonJae <duswo9595@naver.com>
2022-01-13 00:04:58 +0900

    habit tracker

--- src/components/habits.jsx
+++ src/components/habits.jsx
@@ -3,7 +3,7 @@ import Habit from './habit';
 import HabitAddForm from './habitAddForm';
 
 class Habits extends Component {
-  handleIncrement = (habit) => {
+  handleIncrement = (habit) => {               
     this.props.onIncrement(habit);
   };
  
